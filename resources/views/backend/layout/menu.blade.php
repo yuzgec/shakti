a14e9a3f57662c62c7c74af1c026f8eadbb557a5
yuzgec <olcayy@gmail.com>
2024-06-14 10:23:33 +0300

    Fix

--- resources/views/backend/layout/menu.blade.php
+++ resources/views/backend/layout/menu.blade.php
@@ -66,7 +66,10 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 6 15 12 9 18" /></svg>
                                 Somatik Yas
                             </a>
-                           
+                            <a class="dropdown-item" href="{{route('service.index', 'category=12&name=Sinir Sistemi')}}" >
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 6 15 12 9 18" /></svg>
+                                Sinir Sistemi
+                            </a>
                             <a class="dropdown-item" href="{{route('service-categories.index')}}" >
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 6 15 12 9 18" /></svg>
                                 Hizmet Kategori Yönetimi
